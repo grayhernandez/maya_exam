592a437319df83522e3db415db5670dfcbdd0f77
Gray Hernandez <floydrusselh@gmail.com>
2026-03-02 16:28:05 +0800

    Fixed the domain layer and the network client

--- lib/domain/entities/user.dart
+++
@@ -1,5 +0,0 @@
-class User {
-  final String username;
-
-  User({required this.username});
-}
--- lib/domain/repositories/transaction_repository.dart
+++ lib/domain/repositories/transaction_repository.dart
@@ -1,8 +1,6 @@
-import 'package:dartz/dartz.dart';
-import '../../core/errors/failures.dart';
 import '../entities/transaction.dart';
 
 abstract class TransactionRepository {
-  Future<Either<Failure, List<Transaction>>> getTransactions();
-  Future<Either<Failure, Transaction>> sendMoney(double amount);
+  Future<List<Transaction>> getTransactions();
+  Future<Transaction> sendMoney(double amount);
 }
